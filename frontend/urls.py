360df0ebcaba5e06b88acd8f0efcb3ae3e08461e
Isala Piyarisi <code@isala.me>
2020-10-10 18:52:16 +0530

    Improved lighthouse score

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-
+from django.views.generic.base import RedirectView
 from . import views
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -11,4 +13,5 @@ urlpatterns = [
     path('excursions', views.excursions, name='excursions'),
     path('gallery', views.gallery, name='gallery'),
     path('reservation', views.reservation, name='reservation'),
+    path(r'^favicon\.ico$', favicon_view),
 ]
