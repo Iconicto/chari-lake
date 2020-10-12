d18cf91aacf5398e8186ed76187946a4d92b9c0a
Isala Piyarisi <code@isala.me>
2020-10-12 19:11:52 +0530

    Added New images

--- ChariLake/urls.py
+++ ChariLake/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     path('', include('frontend.urls')),
     path('admin/', admin.site.urls),
+    path(r'favicon\.ico', favicon_view),
 ]
